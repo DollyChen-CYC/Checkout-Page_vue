847568c72291acb7c9245613aa1e5c8db55014ab
DollyChen_CYC <dolly726033@gmail.com>
2021-11-18 14:23:14 +0800

    feat: add and import package "VueSweetalert2"

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import './assets/stylesheets/scss/all.scss'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 Vue.config.productionTip = false
 
@@ -11,3 +13,5 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(VueSweetalert2);
